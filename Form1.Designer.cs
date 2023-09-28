8f149236a7c0b2893696d30b379f29326a59e8a6
jtate <jtate.cc@outlook.com>
2023-09-28 19:27:04 +0100

    Added Class for FileWithID and basic form controls

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,20 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.browseFileBtn = new System.Windows.Forms.Button();
+            this.fileListBox = new System.Windows.Forms.ListBox();
+            this.addFileBtn = new System.Windows.Forms.Button();
+            this.filePathTb = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // browseFileBtn
+            // 
+            this.browseFileBtn.Location = new System.Drawing.Point(660, 586);
+            this.browseFileBtn.Name = "browseFileBtn";
+            this.browseFileBtn.Size = new System.Drawing.Size(59, 59);
+            this.browseFileBtn.TabIndex = 0;
+            this.browseFileBtn.Text = "...";
+            this.browseFileBtn.UseVisualStyleBackColor = true;
+            this.browseFileBtn.Click += new System.EventHandler(this.browseFileBtn_Click);
+            // 
+            // fileListBox
+            // 
+            this.fileListBox.FormattingEnabled = true;
+            this.fileListBox.ItemHeight = 20;
+            this.fileListBox.Location = new System.Drawing.Point(207, 116);
+            this.fileListBox.Name = "fileListBox";
+            this.fileListBox.SelectionMode = System.Windows.Forms.SelectionMode.None;
+            this.fileListBox.Size = new System.Drawing.Size(512, 444);
+            this.fileListBox.TabIndex = 1;
+            // 
+            // addFileBtn
+            // 
+            this.addFileBtn.Location = new System.Drawing.Point(564, 586);
+            this.addFileBtn.Name = "addFileBtn";
+            this.addFileBtn.Size = new System.Drawing.Size(58, 59);
+            this.addFileBtn.TabIndex = 2;
+            this.addFileBtn.Text = "+";
+            this.addFileBtn.UseVisualStyleBackColor = true;
+            this.addFileBtn.Click += new System.EventHandler(this.addFileBtn_Click);
+            // 
+            // filePathTb
+            // 
+            this.filePathTb.Location = new System.Drawing.Point(207, 586);
+            this.filePathTb.MinimumSize = new System.Drawing.Size(0, 2);
+            this.filePathTb.Multiline = true;
+            this.filePathTb.Name = "filePathTb";
+            this.filePathTb.Size = new System.Drawing.Size(323, 59);
+            this.filePathTb.TabIndex = 3;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1640, 1306);
+            this.ClientSize = new System.Drawing.Size(1956, 1450);
+            this.Controls.Add(this.filePathTb);
+            this.Controls.Add(this.addFileBtn);
+            this.Controls.Add(this.fileListBox);
+            this.Controls.Add(this.browseFileBtn);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button browseFileBtn;
+        private System.Windows.Forms.ListBox fileListBox;
+        private System.Windows.Forms.Button addFileBtn;
+        private System.Windows.Forms.TextBox filePathTb;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,85 @@ namespace FileInfoScanner
 {
     public partial class Form1 : Form
     {
+
+        private List<FileWithID> fileList = new List<FileWithID>();
         public Form1()
         {
             InitializeComponent();
         }
+
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+        // Opens File Dialog and allows user to pick file, and filter by extension.
+        private void browseFileBtn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog(); // new file dialog object
+            openFileDialog.Title = "Select a File";
+            openFileDialog.Filter = "All Files (*.*)|*.*";  // allowing for extension filtering
+            openFileDialog.CheckFileExists = true;
+            openFileDialog.CheckPathExists = true;
+            openFileDialog.Multiselect = true; // convenient to allow multiple file selection
+
+            // TODO: Add Ability to Select a directory
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK) // validate that user has selected a file
+            {
+                if (openFileDialog.FileNames.Length > 0)
+                {
+                    // Add selected files to the ListBox
+                    foreach (string selectedFile in openFileDialog.FileNames)
+                    {
+                        FileWithID newFile = new FileWithID(selectedFile);
+                        fileList.Add(newFile);
+                        fileListBox.Items.Add(newFile);
+                    }
+                }
+            }
+
+
+        }
+        // allows user to directly enter the path into the text entry and add to the file lit box.
+        private void addFileBtn_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(filePathTb.Text))
+            {
+                FileWithID newFile = new FileWithID(filePathTb.Text);
+                fileList.Add(newFile);
+                fileListBox.Items.Add(newFile);
+            }
+            else
+            {
+                DialogResult err;
+                err = MessageBox.Show("No File Entered or Specified File Does Not Exist");
+            }
+        }
+    }
+}
+
+// keep an oop approach and havin a custom class for files with id;s
+public class FileWithID
+{
+    public string ID { get; set; }
+    public string filePath { get; set; }
+
+
+    public FileWithID(string filepath)
+    {
+        ID = GenerateUniqueId();
+        filePath = filepath;
+    }
+
+    public override string ToString()
+    {
+        return $"{filePath} (ID: {ID})";
+    }
+
+    // Generate a unique ID using te Guid module.
+    private string GenerateUniqueId()
+    {   
+        return Guid.NewGuid().ToString();
     }
 }
